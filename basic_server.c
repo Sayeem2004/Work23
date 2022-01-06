eb8204c021cfbe4e983b26a05b12e0495481e493
Mohammad Khan <MohammadSSKC@gmail.com>
2022-01-05 22:57:08 -0500

    Create basic_server.c

---
+++ basic_server.c
@@ -0,0 +1,103 @@
+#include "pipe_networking.h"
+
+// Variable Declarations
+char *buff;
+int client_socket, frk;
+
+void server_exit() {
+    if (frk == 0) { // Child
+        // Closing Socket
+        int err = close(client_socket);
+        if (err == -1) {
+            print_error(-1, "Sub-Server: Unable To Close Client Socket");
+            return;
+        } else printf("Sub-Server: Successfully Closed Client Socket");
+
+        // Exiting Function
+        free(buff);
+        printf("Sub-Server: Successfully Shut Down\n");
+        exit(0);
+    } else { // Parent
+        // Closing Socket
+        int err = close(client_socket);
+        if (err == -1) {
+            print_error(-1, "Server: Unable To Close Client Socket");
+            return;
+        } else printf("Server: Successfully Closed Client Socket");
+
+        // Exiting Function
+        free(buff);
+        printf("Server: Successfully Shut Down\n");
+        exit(0);
+    }
+}
+
+static void sighandler(int signo) {
+    // SIGINT Case
+    if (signo == SIGINT) {
+        // Exiting Normally
+        server_exit();
+    }
+
+    // Exiting Function
+    return;
+}
+
+int main() {
+    // Signal Handling
+    signal(SIGINT, sighandler);
+
+    // Initializing Buffer
+    buff = calloc(BUFFER_SIZE, sizeof(char));
+
+    // Persistent Process
+    while (1) {
+        // Starting Handshake
+        client_socket = server_handshake();
+        if (client_socket == -1) {
+            reset_server_handshake(client_socket);
+            continue;
+        }
+
+        // Forking Server
+        frk = fork();
+
+        if (frk == 0) { // Child Process
+            // Constantly Runs
+            while (1) {
+                // Reading From User To Worker Pipe
+                int err1 = read(client_socket, buff, BUFFER_SIZE);
+                if (err1 == -1) {
+                    print_error(-1, "Unable To Read Input From Client");
+                    continue;
+                }
+                if (err1 == 0) break;
+
+                // String Manipulation (Upper Case)
+                int i;
+                for (i = 0; i < BUFFER_SIZE; i++) {
+                    if (buff[i] == '\0') break;
+                    buff[i] = toupper(buff[i]);
+                }
+
+                // Writing To Worker To User Pipe
+                int err2 = write(client_socket, buff, BUFFER_SIZE);
+                if (err2 == -1) {
+                    print_error(-1, "Unable To Write Output To Client");
+                    continue;
+                }
+            }
+
+            // Exiting Child Process
+            server_exit();
+        } else if (frk ==  -1) { // Error
+            print_error(-1, "Server: Unable To Fork Sub-Process");
+        } else { // Parent Process
+            // Resetting Handshake
+            reset_server_handshake(client_socket);
+        }
+    }
+
+    // Exiting Normally
+    server_exit();
+}
